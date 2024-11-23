7da4ae04ba608a8f6290d3333c3834cee14f1119
Bruno Ribeiro <capicapi1515@gmail.com>
2024-11-23 20:18:41 +0000

    correction to register

--- models/userModel.js
+++ models/userModel.js
@@ -10,7 +10,7 @@ const userSchema = new Schema({
     last_name: {type: String, required: false},
     register_date: {type: String, required: true},
     last_sign_in: {type: String, required: true},
-    birthdate: {type: String, required: true},
+    birthdate: {type: String, required: false},
     document_name: {type: String, required: false},
     active: {type: Boolean, required: true, default: true}
 },{collection:'users'});
