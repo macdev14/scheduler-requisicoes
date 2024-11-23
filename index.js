bd05e85626d616fd31bd283416bd7a482d976e21
Bruno Ribeiro <capicapi1515@gmail.com>
2024-11-23 12:01:23 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+//required app modules
+const express = require("express");
+const path = require("path");
+require('dotenv').config();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./public/swagger/swagger.json');
+
+
+const app = express();
+let port=process.env.PORT || 3000;
+let uri=process.env.MONGO_CONNECTION_STRING;
+
+//database connection
+mongoose.connect(uri).then(() => {
+    console.log("Connected to the database");
+}).catch((err) => {
+    console.log(err);
+})
+
+//middleware
+app.use(bodyParser.json()); //parse application/json and application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true })); //allowing for extended syntax (i.e. arrays, objects, nested objects, etc.)
+
+//routes
+app.use('/', express.static(path.join(__dirname, 'public'))); //available static route at http://localhost:5565
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); //serve api documentation
+app.use('/api-docjs', express.static('./public/apidocjs')); //available route at http://localhost:5565/api-docjs/
+app.use("/api", require("./routes/userRoute")); //user route
+
+app.listen(port, () => {
+    console.log("Server running on port: " + port);
+})
---
+++ models/userModel.js
@@ -0,0 +1,18 @@
+'use strict';
+//database schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    email: {type: String, required: false, unique: false},
+    first_name: {type: String, required: false},
+    last_name: {type: String, required: false},
+    register_date: {type: String, required: true},
+    last_sign_in: {type: String, required: true},
+    birthdate: {type: String, required: true},
+    document_name: {type: String, required: false},
+    active: {type: Boolean, required: true, default: true}
+},{collection:'users'});
+const User = mongoose.model("User", userSchema);
+module.exports = User;
