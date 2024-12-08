6126478ac6cda6f8c11c876bac03b40c987616c1
Lauro <lauropimentel12@gmail.com>
2024-12-08 15:47:49 +0000

    push

---
+++ entities/RequisitionEntity.js
@@ -0,0 +1,23 @@
+exports.RequisitionEntity = class RequisitionEntity {
+    constructor(requisition) {
+        this.user_id = requisition.user_id;
+        this.event_name = requisition.event_name;
+        this.start_date = requisition.start_date || new Date();
+        this.end_date = requisition.end_date || null;
+        this.products = requisition.products || [];
+        this.active = requisition.active !== undefined ? requisition.active : true;
+        this.approved = requisition.approved !== undefined ? requisition.approved : true;
+    }
+
+    async validator() {
+        if (!this.event_name  || !this.start_date || !this.end_date || !this.products.length>0 ) {
+            return { status: 400, message: "All required fields must be provided (event_name, start_date, end_date, products, active, approved)." };
+        }
+
+        if (new Date(this.start_date) > new Date(this.end_date)) {
+            return { status: 400, message: "start_date cannot be later than end_date." };
+        }
+
+        return { status: 200, message: "Requisition is valid." };
+    }
+};
---
+++ entities/RequisitionJwtEntity.js
@@ -0,0 +1,13 @@
+exports.RequisitionJwtEntity = class RequsitionJwtEntity {
+    constructor(requisition, token) {
+        this.token = token;
+        this.name = requisition.name;
+        this.user_id = requisition.user_id;
+        this.insert_date = requisition.insert_date || new Date();
+        this.description = requisition.description || null;
+        this.start_date = requisition.start_date || null;
+        this.end_date = requisition.end_date || null;
+        this.product_id = requisition.product_id || null;
+        this.active = requisition.active !== undefined ? requisition.active : true;
+    }
+};
