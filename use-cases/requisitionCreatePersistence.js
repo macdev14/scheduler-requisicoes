4b0a471e466e1c072c8cbf32d0910128a4b8f9a5
Lauro <lauropimentel12@gmail.com>
2024-12-09 23:47:27 +0000

    approval

--- use-cases/requisitionCreatePersistence.js
+++ use-cases/requisitionCreatePersistence.js
@@ -14,32 +14,42 @@ const parseDate = (dateString) => {
 const Requisition = mongoose.model("Requisition");
 
 
-exports.requisitionCreatePersistence = async (requisition) => {
-    //console.log("event", event);
-   
-
+exports.requisitionCreatePersistence = async (requisition) => {  
     try {
         const { event_name, start_date, end_date, approved, active, products, token } = requisition;
         
         if (!event_name || !start_date || !end_date || !token || products.length == 0) {
             return { status: 400, message: "token, event name, products, start_date, and end_date are required" };
         }
-
         if (event_name.length > process.env.EVENT_NAME_MAX_SIZE) {
             return { status: 400, message: `event name must be less than ${process.env.EVENT_NAME_MAX_SIZE} characters` };
         }
-
         const parsedStartDate = parseDate(start_date);
         const parsedEndDate = parseDate(end_date);
-
         if (!parsedStartDate || !parsedEndDate) {
             return { status: 400, message: "Invalid date format. Use dd/mm/yyyy or yyyy/mm/dd" };
         }
- 
         try {
             const decoded = jwt.verify(token, process.env.SECRET_KEY);
-
             if (decoded.role == process.env.ROLE_ADMIN || decoded.role == process.env.ROLE_MANAGER || decoded.role == process.env.ROLE_EXTERNAL) {
+                const requisitions = await Requisition.find({ active,
+                    start_date: { $gte: parsedStartDate },  // Greater than or equal to start date
+                    end_date: { $lte: parsedEndDate }       // Less than or equal to end date
+                 });
+                const has = false
+                requisitions.forEach(requisition => { 
+                        requisition.products.forEach(product_db => {
+                            products.forEach(product => {
+
+                                if(product_db.id == product.id && !product_db.quantity < product.quantity){
+                                    has = true
+                                }
+                            })
+                        })
+                })
+                if(has){
+                    return { status: 400, message: "Products already reserved" };
+                }
                 const createRequisition = {
                     user_id: decoded.id,
                     event_name,
@@ -49,9 +59,7 @@ exports.requisitionCreatePersistence = async (requisition) => {
                     active,
                     products
                 };
-              
                 const result = await Requisition.create(createRequisition);
-               
                 return { status: 201, id: result.id , message: "Requisition created successfully" };
             }
             return ({status: 403, message: "Access denied. Insufficient permissions."});
@@ -59,7 +67,6 @@ exports.requisitionCreatePersistence = async (requisition) => {
             console.log("err", err);
             return ({status: 403, message: "Access denied"});
         }
-
     } catch (error) {
         console.log("error", error);
         return { status: 500, message: "Something went wrong" };
